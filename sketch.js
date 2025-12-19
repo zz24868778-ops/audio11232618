d850dd97395e2f065a1c5bc598d6ac9ce2c5c43f
zz24868778-ops <zz24868778@gmail.com>
2025-12-19 19:19:36 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,6 +28,7 @@ function setup() {
   createCanvas(320, 240);
   // Start classifying
   // The sound model will continuously listen to the microphone
+  ThunkableWebviewerExtension.postMessage("ready");
   classifier.classify(gotResult);
 }
 
@@ -50,5 +51,6 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
-
+ThunkableWebviewerExtension.postMessage(label);
 }
+
